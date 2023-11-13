4c117df176f9dc2790b9bda9da43cb4b10ec9017
Unto Pulkkinen <untop@metropolia.fi>
2023-11-13 15:24:45 +0200

    Initial Commit

---
+++ MapChat/MapChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapChatApp.swift
+//  MapChat
+//
+//  Created by Unto Pulkkinen on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
