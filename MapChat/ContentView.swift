6d2b0b6d0268ed095f7ba45334cd80bddbcfb905
Glatus <janipudas1@gmail.com>
2023-11-15 20:33:11 +0200

    x

--- MapChat/ContentView.swift
+++ MapChat/ContentView.swift
@@ -8,20 +8,30 @@
 import MapKit
 import SwiftUI
 
-struct ContentView: View {
+struct MapView: View {
+    @StateObject private var viewModel = MapViewModel()
     
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
-    
- 
     var body: some View {
-            Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
-                        
-        }
+        Map(coordinateRegion: $viewModel.region, showsUserLocation: true, userTrackingMode: .constant(.follow))
+            .ignoresSafeArea()
+            .accentColor(Color(.systemPink))
+            .onAppear() {
+                viewModel.checkIfLocationServicesEnabled()
+            }
+        Button(action: {
+                        viewModel.centerMapOnUserLocation()
+                    }) {
+                        Image(systemName: "location.north.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 32, height: 32)
+                    }
+                    .padding()
     }
-
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MapView()
     }
 }
