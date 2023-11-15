5ac7c8c89f87e859671565e54941942dc8570551
AksuMetroMaster <aleksis.nummela@gmail.com>
2023-11-15 15:46:33 +0200

    Mapkit visible, asks location and centers on user

--- MapChat/MapChatApp.swift
+++ MapChat/MapChatApp.swift
@@ -6,9 +6,18 @@
 //
 
 import SwiftUI
-
+import MapKit
 @main
 struct MapChatApp: App {
+    private let manager = CLLocationManager()
+    init (){
+        askForLocation()
+    }
+    func askForLocation(){
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
