65f4ea15b395641a8d2a5b75b7ca731270c51bbf
AksuMetroMaster <aleksis.nummela@gmail.com>
2023-11-20 09:50:00 +0200

    A beautiful button

--- MapChat/ContentView.swift
+++ MapChat/ContentView.swift
@@ -12,12 +12,27 @@ struct ContentView: View {
     
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
     
- 
     var body: some View {
+        ZStack(alignment: .bottom){
             Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
-                        
+            
+            HStack(){
+                Spacer()
+                Button(action: {
+                    print("Round Action")
+                }) {
+                    Image(systemName: "plus").font(.system(size: 30))
+                        .frame(width: 85, height: 85)
+                        .foregroundColor(Color.black)
+                        .background(Color.white)
+                        .clipShape(Circle())
+                }}.padding()
         }
     }
+    
+    
+}
+
 
 
 struct ContentView_Previews: PreviewProvider {
