7f6395464d6b62451d651cdc063af70dc38186a5
Glatus <janipudas1@gmail.com>
2023-11-21 18:51:04 +0200

    Selvemmät kartta funktiot ja periaatteessa toimiva viesti jos ei lupaa sijaintiin👀

--- MapChat/ContentView.swift
+++ MapChat/ContentView.swift
@@ -10,23 +10,82 @@ import SwiftUI
 
 struct MapView: View {
     @StateObject private var viewModel = MapViewModel()
+    @State private var showingAlert = false
     
     var body: some View {
-        ZStack(alignment: .bottomTrailing) {
+        ZStack(alignment: .bottom) {
             Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
                 .ignoresSafeArea()
                 .accentColor(Color(.systemPink))
                 .onAppear() {
-                    viewModel.checkIfLocationServicesEnabled()
+                    viewModel.authorizationResult = nil
+                    viewModel.centerMapOnUserLocation()
                 }
-            Button(action: {
-                viewModel.centerMapOnUserLocation()
-            }) {
-                Image(systemName: "location.north.circle.fill")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 32, height: 32)
-                    .position()
+                .onChange(of: viewModel.authorizationResult) { newAuthResult in
+                    switch newAuthResult {
+                    case .denied, .restricted:
+                        showingAlert = true
+                    default:
+                        showingAlert = false
+                    }
+                }
+                .alert(isPresented: $showingAlert) {
+                    switch viewModel.authorizationResult {
+                    case .denied:
+                        return Alert(
+                            title: Text("Location Permission Denied"),
+                            message: Text("Apps location permission denied"),
+                            dismissButton: .default(Text("OK"))
+                        )
+                    case .restricted:
+                        return Alert(
+                            title: Text("Location Restricted"),
+                            message: Text("Phones location restricted"),
+                            dismissButton: .default(Text("OK"))
+                        )
+                    default:
+                        return Alert(
+                            title: Text("Default Title"),
+                            message: Text("Default Message"),
+                            dismissButton: .default(Text("OK"))
+                        )
+                    }
+                }
+            VStack(){
+                HStack(){
+                    Button(action: {
+                        print("da menu")
+                    }) {
+                        Image(systemName: "list.bullet").font(.system(size: 35)).foregroundColor(Color.black)}
+                    Spacer()
+                    Button(action: {
+                        print("looking for you")
+                    }) {
+                        Image(systemName: "magnifyingglass").font(.system(size: 25)).foregroundColor(Color.black).frame(width: 45, height: 45).overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.black, lineWidth: 2)
+                        )}
+                }.padding()
+                Spacer()
+                HStack(){
+                    Button(action: {
+                        viewModel.centerMapOnUserLocation()
+                    }) {
+                        Image(systemName: "location.north.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 32, height: 32).padding()
+                        Spacer()
+                        Button(action: {
+                            print("plus perfect")
+                        }) {
+                            Image(systemName: "plus").font(.system(size: 30))
+                                .frame(width: 85, height: 85)
+                                .foregroundColor(Color.black)
+                                .background(Color.white)
+                                .clipShape(Circle())
+                        }}.padding()
+                }
             }
         }
     }
