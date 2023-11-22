6b702fdf6fe97ca9fa416a8b7f40f8127dac58f2
Unto Pulkkinen <untop@metropolia.fi>
2023-11-22 14:27:29 +0200

    siisti lista

---
+++ MapChat/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  MapChat
+//
+//  Created by Unto Pulkkinen on 22.11.2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
