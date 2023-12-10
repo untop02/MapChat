d7431c019e18099c00587c1efc722f9398fa4a0d
Unto Pulkkinen <untop@metropolia.fi>
2023-12-10 21:45:36 +0200

    suomennos ja mustat tekstit

--- MapChat/Views/CardView.swift
+++ MapChat/Views/CardView.swift
@@ -21,11 +21,14 @@ struct CardView: View {
                 Text(location.title ?? "")
                     .font(.headline)
                     .accessibilityAddTraits(.isHeader)
+                    .foregroundColor(.black)
                 Spacer()
                 HStack {
                     Text(location.description ?? "")
+                        .foregroundColor(.black)
                     Spacer()
                     Label("\(CLLocation(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude).distance(from: locationManager.location ?? CLLocation(latitude:60.19, longitude: 24.94))) m", systemImage: "figure.walk")
+                        .foregroundColor(.black)
                 }
                 .font(.caption)
             }.padding()
